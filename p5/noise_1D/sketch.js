8ac74766866350ba090930f426f0c1f7f2b225ad
fiona <fi.carty@gmail.com>
2018-09-15 23:41:29 -0700

    added noise ball for the y axis as well

--- p5/noise_1D/sketch.js
+++ p5/noise_1D/sketch.js
@@ -1,6 +1,9 @@
 var xoff = 0.0;
 var xincrement = 0.01;
 
+var yoff = 0.0;
+var yincrement = 0.01;
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     background(0);
@@ -12,16 +15,28 @@ function draw() {
     fill(0,10);
     rect(0,0,width,height);
     
-    // var n = random(0, width); // try this line instead of noise
+    // var v = random(0, width); // try this line instead of noise
     
     // Get a noise value on xoff and scale 
     // it according to the window's width
     var n = noise(xoff)*width;
     
+    var t = noise(yoff)*height;
+    
+    
     // with each cycle, increment xoff
     xoff += xincrement;
+    yoff += yincrement;
     
     // draw the ellipse at the value produced by perlin noise 
     fill(400);
-    ellipse(n, height/2, 64, 64);
+    ellipse(n, height/2, 100, 100);
+    
+    fill(400);
+    ellipse(n, height/4, 100, 100);
+    
+    fill(400);
+    ellipse(width/2, t, 100, 100);
+    
 }
+
